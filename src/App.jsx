344ace3dee56e407d867359a6df5155272039a3c
miloszKubiak <milypan69@gmail.com>
2022-07-05 15:17:39 +0200

    setup project and create main components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo";
+
+const CLIENT_ID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const MAIN_URL = `https://api.unsplash.com/photos/`;
+const SEARCH_URL = `https://api.unsplash.com/search/photos/`;
+
+function App() {
+	return <h2>dupa</h2>;
+}
+
+export default App;
