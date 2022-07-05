e46167351958c3ac2598069c6a2cced87368a8e8
miloszKubiak <milypan69@gmail.com>
2022-07-05 15:52:43 +0200

    create fetch function

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,28 @@ const MAIN_URL = `https://api.unsplash.com/photos/`;
 const SEARCH_URL = `https://api.unsplash.com/search/photos/`;
 
 function App() {
+	const [loading, setLoading] = useState(false);
+	const [photos, setPhotos] = useState([]);
+
+	const fetchImages = async () => {
+		setLoading(true);
+		let url;
+		url = `${MAIN_URL}${CLIENT_ID}`;
+
+		try {
+			const response = await fetch(url);
+			const data = await response.json();
+			console.log(data);
+		} catch (error) {
+			setLoading(false);
+			console.log(error.response);
+		}
+	};
+
+	useEffect(() => {
+		fetchImages();
+	}, []);
+
 	return <h2>dupa</h2>;
 }
 
